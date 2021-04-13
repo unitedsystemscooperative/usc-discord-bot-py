b79841a2fd2762b2dae25d59bcf227720b95c2a5
Zachary <Zachary@admiralfeb.net>
2021-04-13 21:38:05 +0000

    mv to repl

--- commands.py
+++ commands.py
@@ -11,7 +11,10 @@ def load_commands(bot: Bot):
     test_guild_ids = [181594027945099264]
     guild_ids = [662439414152167434]
 
-    @slash.slash(name="bot_announce_myself", description="Can only be used by Admiralfeb. Announces the bot.", guild_ids=test_guild_ids)
+    @slash.slash(
+        name="bot_announce_myself",
+        description="Can only be used by Admiralfeb. Announces the bot.",
+        guild_ids=test_guild_ids)
     async def _bot_announce_myself(ctx: SlashContext):
         author = ctx.author
         if author.id != 138029803214209025:
@@ -22,33 +25,52 @@ def load_commands(bot: Bot):
 
     @slash.slash(name='can_i_sc_there', guild_ids=guild_ids)
     async def _can_i_sc_there(ctx: SlashContext):
-        await ctx.send("Check this out: http://caniflytothenextstarinelitedangero.us/")
+        await ctx.send(
+            "Check this out: http://caniflytothenextstarinelitedangero.us/")
 
-    @slash.slash(name='fsd_booster', description=''' Links to Exegious' video on how to unlock the Guardian FSD Booster ''', guild_ids=guild_ids)
+    @slash.slash(
+        name='fsd_booster',
+        description=
+        ''' Links to Exegious' video on how to unlock the Guardian FSD Booster ''',
+        guild_ids=guild_ids)
     async def _fsd_booster(ctx: SlashContext):
-        await ctx.send("Here's how to unlock the guardian fsd booster: https://youtu.be/J9C9a00-rkQ")
+        await ctx.send(
+            "Here's how to unlock the guardian fsd booster: https://youtu.be/J9C9a00-rkQ"
+        )
 
     @slash.slash(
         name='neutron',
         description='Gives tutorials on how to use the neutron highway',
         guild_ids=guild_ids,
-        options=[create_option(
-            name='option', description="Choose what you'd like to know about the Neutron Highway",
-            option_type=3, required=True,
-            choices=[
-                create_choice(value='img', name='Image Tutorial'),
-                create_choice(value='spansh', name="Spansh's Website"),
-                create_choice(value='desktop', name='Download an app'),
-            ]
-        )])
+        options=[
+            create_option(
+                name='option',
+                description=
+                "Choose what you'd like to know about the Neutron Highway",
+                option_type=3,
+                required=True,
+                choices=[
+                    create_choice(value='img', name='Image Tutorial'),
+                    create_choice(value='spansh', name="Spansh's Website"),
+                    create_choice(value='desktop', name='Download an app'),
+                ])
+        ])
     async def _neutron(ctx: SlashContext, option: str):
         if option == 'img':
             await ctx.send("https://i.imgur.com/gg6n5VM.jpg")
         elif option == 'spansh':
-            await ctx.send("Plot neutron highway routes online here: https://www.spansh.co.uk/plotter")
+            await ctx.send(
+                "Plot neutron highway routes online here: https://www.spansh.co.uk/plotter"
+            )
         else:
-            await ctx.send("Desktop client for neutron routing here: https://github.com/winneon/neutron")
+            await ctx.send(
+                "Desktop client for neutron routing here: https://github.com/winneon/neutron"
+            )
 
-    @slash.slash(name="squadron", description="gives in-game squadron and suggests inara", guild_ids=guild_ids)
+    @slash.slash(name="squadron",
+                 description="gives in-game squadron and suggests inara",
+                 guild_ids=guild_ids)
     async def _squadron(ctx: SlashContext):
-        await ctx.send("To join the squadron, go to your right hand panel, click on the squadron button, search **USPC** in the search bar, and you should find us!\n\nAfter you've joined in-game, you are also free to join our inara squadron!\nhttps://inara.cz/squadron/7028/")
+        await ctx.send(
+            "To join the squadron, go to your right hand panel, click on the squadron button, search **USPC** in the search bar, and you should find us!\n\nAfter you've joined in-game, you are also free to join our inara squadron!\nhttps://inara.cz/squadron/7028/"
+        )
