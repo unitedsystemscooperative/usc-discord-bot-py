e5386a17c607de31efb0e644e3419f1322dfc434
Zachary Austin <Zachary@admiralfeb.net>
2021-04-27 07:18:37 +0000

    feat: bgs tick detected

--- cogs/bgs.py
+++ cogs/bgs.py
@@ -1,7 +1,10 @@
-import asyncio
-import logging
 
-import websocket
+import asyncio
+import datetime
+import logging
+import math
+
+import requests
 from discord import Embed, TextChannel
 from discord.ext import commands, tasks
 from discord.ext.commands.bot import Bot
@@ -9,23 +12,79 @@ from mongo import get_value, set_value
 
 logging.basicConfig(level=logging.INFO)
 
-TICK_SOCKET = 'http://tick.phelbore.com:31173'
+TICK_API = 'https://elitebgs.app/api/ebgs/v5/ticks'
+FACTION_API = 'https://elitebgs.app/api/ebgs/v5/factions?name=United%20Systems%20Cooperative'
 
 
 class BGSCog(commands.Cog):
+
     def __init__(self, bot: Bot):
         self.bot = bot
         print("Load BGS Cog")
-        self.wsapp = websocket.WebSocketApp(
-            TICK_SOCKET, on_message=self.on_message)
+        self.watch_for_tick.start()
 
     def cog_unload(self):
-        self.wsapp.close()
+        self.watch_for_tick.stop()
 
-    async def on_message(self, wsapp, message):
+    @tasks.loop(hours=1)
+    async def watch_for_tick(self):
+        tick_time: str = requests.get(TICK_API).json()[0]['time']
+        tick_time = tick_time.replace('Z', '')
+        print(tick_time)
+        last_tick = get_value('lastTick')
+
+        if tick_time != last_tick:
+            await self.send_tick_message(tick_time)
+            set_value('lastTick', tick_time)
+            await asyncio.sleep(3600)
+            await self.send_systems_message()
+
+    @watch_for_tick.before_loop
+    async def _wait_for_bot(self):
+        print('tick watcher waiting for bot...')
+        await self.bot.wait_until_ready()
+
+    async def send_tick_message(self, tick: str):
+        ''' Builds and sends the 'Tick Detected' message '''
+        tick_time = datetime.datetime.fromisoformat(tick)
+        latest_tick_time = tick_time.time().isoformat(timespec='minutes')
+        latest_tick_date = tick_time.date().isoformat()
+
+        embed = Embed(title='Tick Detected')
+        embed.add_field(name="Latest Tick at",
+                        value=f"{latest_tick_time} UTC - {latest_tick_date}")
+        embed.timestamp = datetime.datetime.now()
         channel: TextChannel = self.bot.get_channel(831261490258509935)
-        await channel.send(message)
-        print(message)
+        await channel.send(embed=embed)
 
-    def log_message(self, message: str):
-        logging.info(f"Message: {message}")
+    async def send_systems_message(self):
+        ''' Builds and sends the current system information '''
+        data = requests.get(FACTION_API).json()
+        presences = data['docs'][0]['faction_presence']
+        channel: TextChannel = self.bot.get_channel(831261490258509935)
+
+        i = 1
+        embed = Embed(title='BGS Report')
+        for presence in presences:
+            if i == 25:
+                await channel.send(embed=embed)
+                embed = Embed(title='BGS Report Cont')
+                i = 1
+            system_name = presence['system_name']
+            influence = "{:.1%}".format(presence['influence'])
+            active_states = presence['active_states']
+            pending_states = presence['pending_states']
+            updated_at = presence['updated_at']
+            faction_info = self.build_faction_info(
+                influence, active_states, pending_states, updated_at)
+            embed.add_field(name=system_name, value=faction_info)
+
+            i += 1
+
+        if len(presences) <= 25:
+            await channel.send(embed=embed)
+
+    @staticmethod
+    def build_faction_info(influence: str, active_states: list[dict], pending_states: list[dict], updated_at: str) -> str:
+
+        pass
--- cogs/powerplay.py
+++ cogs/powerplay.py
@@ -16,7 +16,7 @@ class Powerplay(commands.Cog):
     @tasks.loop(hours=1.0)
     async def powerplay_loop(self):
         time = datetime.utcnow()
-        if time.weekday() == 3:
+        if time.weekday() == 2:
             if time.hour == 0:
                 guild: Guild = self.bot.get_guild(662439414152167434)
                 pp_role: Role = discord.utils.get(
